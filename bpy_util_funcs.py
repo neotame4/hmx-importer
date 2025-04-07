6a46ff0ca5b73f672e11eca133fc1ec8a86dc653
neotame4 <155486890+neotame4@users.noreply.github.com>
2025-04-07 11:47:31 -0400

    fixed math and added signed_compressed_vec4

--- bpy_util_funcs.py
+++ bpy_util_funcs.py
@@ -5,22 +5,49 @@ def invert_uv_map(uvs: tuple[float, float]) -> tuple[float, float]:
 def reverse_vector(vector: tuple) -> tuple:
     return (vector[3], vector[2], vector[1], vector[0])
 
+def signed_compressed_vec4(value: int) -> tuple[float, float, float, float]:
+    MAX_2_BIT_SIGNED = (1 << 1) - 1
+    MASK_2_BIT = (1 << 2) - 1
+    MAX_10_BIT_SIGNED = (1 << 9) - 1
+    MASK_10_BIT = (1 << 10) - 1
+    w_bits = int(value >> 30 & MASK_2_BIT)
+    z_bits = int(value >> 20 & MASK_10_BIT)
+    y_bits = int(value >> 10 & MASK_10_BIT)
+    x_bits = int(value & MASK_10_BIT)
+    if x_bits > MAX_10_BIT_SIGNED:
+        x_bits = -1 * (~(x_bits - 1) & (MASK_10_BIT >> 1))
+    if y_bits > MAX_10_BIT_SIGNED:
+        y_bits = -1 * (~(y_bits - 1) & (MASK_10_BIT >> 1))
+    if z_bits > MAX_10_BIT_SIGNED:
+        z_bits = -1 * (~(z_bits - 1) & (MASK_10_BIT >> 1))
+    if w_bits > MAX_2_BIT_SIGNED:
+        w_bits = -1 * (~(w_bits - 1) & (MASK_2_BIT >> 1))
+    x = float(x_bits / float(MAX_10_BIT_SIGNED))
+    y = float(y_bits / float(MAX_10_BIT_SIGNED))
+    z = float(z_bits / float(MAX_10_BIT_SIGNED))
+    w = float(w_bits / float(MAX_2_BIT_SIGNED))
+    return (x, y, z, w)
+
 def unsigned_compressed_vec4(value: int) -> tuple[float, float, float, float]:
-    w_bits = int(value >> 30 & 3)
-    z_bits = int(value >> 20 & 1023)
-    y_bits = int(value >> 10 & 1023)
-    x_bits = int(value & 1023)
-    x = float(x_bits / float(1023))
-    y = float(y_bits / float(1023))
-    z = float(z_bits / float(1023))
-    w = float(w_bits / float(3))
+    MAX_2_BIT_UNSIGNED = (1 << 2) - 1
+    MASK_2_BIT = (1 << 2) - 1
+    MAX_10_BIT_UNSIGNED = (1 << 10) - 1
+    MASK_10_BIT = (1 << 10) - 1
+    w_bits = int(value >> 30 & MASK_2_BIT)
+    z_bits = int(value >> 20 & MASK_10_BIT)
+    y_bits = int(value >> 10 & MASK_10_BIT)
+    x_bits = int(value & MASK_10_BIT)
+    x = float(x_bits / float(MAX_10_BIT_UNSIGNED))
+    y = float(y_bits / float(MAX_10_BIT_UNSIGNED))
+    z = float(z_bits / float(MAX_10_BIT_UNSIGNED))
+    w = float(w_bits / float(MAX_2_BIT_UNSIGNED))
     return (x, y, z, w)
 
 def ps3_weights_math(weights: tuple[int, int, int, int]) -> tuple[float, float, float, float]:
     return (weights[0] / 255.0, weights[1] / 255.0, weights[2] / 255.0, weights[3] / 255.0)
 
 def pos_math(pos: int) -> float:
-    return (pos / 32767.0)
+    return (pos / 32767.0 * 1280)
 
 def pos_math3(pos: tuple[int, int, int]) -> tuple[float, float, float]:
     return ((pos[0] / 32767.0 * 1280), (pos[1] / 32767.0 * 1280), (pos[2] / 32767.0 * 1280))
